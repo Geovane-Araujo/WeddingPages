7dd5d653e70f4d5b3096a9d8d5629636b9dd0b70
nagaminehiro <hiro.nagamini@outlook.com>
2023-08-27 20:42:26 -0300

    alteracoes carrossel
    alteracoes carrossel

--- WeddingPages/src/app/pages/home/components/home-caroussel/home-caroussel.component.ts
+++ WeddingPages/src/app/pages/home/components/home-caroussel/home-caroussel.component.ts
@@ -7,17 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomeCarousselComponent {
   images: string[] = [
-    '../../../assets/images/1.jpeg',
+    // '../../../assets/images/1.jpeg',
     '../../../assets/images/2.jpeg',
-    '../../../assets/images/3.jpeg',
-    '../../../assets/images/4.jpeg',
+    // '../../../assets/images/3.jpeg',
+    // '../../../assets/images/4.jpeg',
     '../../../assets/images/5.jpeg',
     '../../../assets/images/6.jpeg',
     '../../../assets/images/7.jpeg',
     '../../../assets/images/9.jpeg',
     '../../../assets/images/10.jpeg',
     '../../../assets/images/11.jpeg',
-    '../../../assets/images/12.jpeg'
+    // '../../../assets/images/12.jpeg'
   ];
   currentImages: string[] = [];
   currentIndex: number = 0;
