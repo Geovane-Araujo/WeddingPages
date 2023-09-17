d95f1b2451864ff2da05184f535dbdb63dc2c649
JonasMagalhaes <jonasevmagalhaes@gmail.com>
2023-09-17 20:33:43 -0300

    Adicionada lista de acompanhantes

--- WeddingPages/src/app/pages/attendance/components/attendance-confirmation/attendance-confirmation.component.ts
+++ WeddingPages/src/app/pages/attendance/components/attendance-confirmation/attendance-confirmation.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Companion {
+  id: number;
+}
+
 @Component({
   selector: 'app-attendance-confirmation',
   templateUrl: './attendance-confirmation.component.html',
   styleUrls: ['./attendance-confirmation.component.scss']
 })
 export class AttendanceConfirmationComponent {
+  public companionList: Companion[] = [];
 
+  private lastId: number = 0;
+
+  public addCompanion(): void {
+    this.companionList.push({ id: this.lastId++ });
+  }
+
+  public removeCompanion(): void {
+    this.companionList.pop();
+  }
 }
