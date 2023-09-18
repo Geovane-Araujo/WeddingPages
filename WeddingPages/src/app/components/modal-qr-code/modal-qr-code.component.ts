713a182c6976413d9c32290e210a6800c9e6e50c
JonasMagalhaes <jonasevmagalhaes@gmail.com>
2023-09-17 21:45:11 -0300

    Ajustes na geração do QRCODE

--- WeddingPages/src/app/components/modal-qr-code/modal-qr-code.component.ts
+++ WeddingPages/src/app/components/modal-qr-code/modal-qr-code.component.ts
@@ -7,14 +7,19 @@ import { ModalQrCodeService } from './modal-qr-code.service';
   styleUrls: ['./modal-qr-code.component.scss']
 })
 export class ModalQrCodeComponent {
+  public copied: boolean;
+   
   private readonly QRCODE = "00020126580014BR.GOV.BCB.PIX01360207290c-f3f3-4716-b0c2-bb78944e446f5204000053039865802BR5922Messias Silva de Paulo6009SAO PAULO61080540900062240520eVwMuV3HFgaSga923esk630401D8";
+
   constructor(public readonly modalQrCodeService: ModalQrCodeService) {}
 
   public hideModal(): void {
     this.modalQrCodeService.visible.set(false);
+    this.copied = false;
   }
 
   public copyQrCode(): void {
     navigator.clipboard.writeText(this.QRCODE);
+    this.copied = true;
   }
 }
